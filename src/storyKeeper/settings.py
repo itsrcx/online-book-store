0cb4c227d80c88132c57dedc7d258059d26a0d09
raman <ramanchauhanxx@gmail.com>
2023-09-25 23:54:10 +0530

    change to sqlite

--- src/storyKeeper/settings.py
+++ src/storyKeeper/settings.py
@@ -66,13 +66,18 @@ WSGI_APPLICATION = 'storyKeeper.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': '/etc/myconf/my.cnf',
-        },
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'read_default_file': '/etc/myconf/my.cnf',
+#         },
+#     }
+# }
 
 
 # Password validation
