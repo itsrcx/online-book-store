6891e9c488dd84ef90b4394d4734c8e0e07113e3
rcx <ramanchauhanxx@gmail.com>
2023-10-06 11:12:41 +0530

    filters added

--- src/books/forms.py
+++ src/books/forms.py
@@ -9,6 +9,39 @@ class ShippingAddressForm(forms.ModelForm):
 class PlaceOrderForm(forms.Form):
     place_order = forms.BooleanField(widget=forms.HiddenInput, initial=True)
 
+
+class BookFilterForm(forms.Form):
+    author = forms.CharField(
+        required=False,
+        label='',
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'Author'})
+    )
+    min_price = forms.DecimalField(
+        required=False,
+        label='',
+        min_value=0,  
+        max_digits=10,
+        decimal_places=2,
+        widget=forms.NumberInput(attrs={'placeholder': 'Min Price'}))
+    max_price = forms.DecimalField(
+        required=False,
+        label='',
+        min_value=0, 
+        max_digits=10,
+        decimal_places=2,
+        widget=forms.NumberInput(attrs={'placeholder': 'Max Price'})
+    )
+    def clean(self):
+        cleaned_data = super().clean()
+        min_price = cleaned_data.get('min_price')
+        max_price = cleaned_data.get('max_price')
+
+        if min_price is not None and max_price is not None and min_price > max_price:
+            raise forms.ValidationError("Minimum price should be less than or equal to maximum price.")
+
+        return cleaned_data
+
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
--- src/books/views.py
+++ src/books/views.py
@@ -21,8 +21,25 @@ def homeView(request):
     page = request.GET.get('page')
     books = paginator.get_page(page)
     genre = Genre.objects.all().order_by('name')
-    context = {'books': books, 'genre':genre}
     search_book = request.GET.get('search')
+    context = {'books': books, 'genre':genre}
+
+    filter_form = BookFilterForm(request.GET)
+    if filter_form.is_valid():
+        min_price = filter_form.cleaned_data.get('min_price')
+        max_price = filter_form.cleaned_data.get('max_price')
+        author = filter_form.cleaned_data.get('author')
+        filters = Q()
+        books = Book.objects.all().order_by('title')
+        if min_price is not None:
+            filters &= Q(price__gte=min_price)
+        if max_price is not None:
+            filters &= Q(price__lte=max_price)
+        if author:
+            filters &= Q(author__icontains=author)
+        books = Book.objects.filter(filters).order_by('title')
+
+    context = {'books': books, 'genre':genre, 'filter_form':filter_form}
     if search_book:
         books = Book.objects.filter(Q(title__icontains=search_book)|Q(author__icontains=search_book)).distinct()
         paginator = Paginator(books, 9)
