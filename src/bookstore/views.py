5f0bdf6e1ae14ddaf45d29e85dfd9e7944ed2ad1
joyboy <ramanchauhanxx@gmail.com>
2024-01-08 18:33:21 +0530

    ..

--- src/bookstore/views.py
+++ src/bookstore/views.py
@@ -6,9 +6,7 @@ from .serializers import BookSerializer
 class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]  # Requires authentication
 
 class BookDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]  # Requires authentication
