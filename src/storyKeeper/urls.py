cc64a9e4ebfa84746718e6822563c60f2ceb5bf7
Raman Chauhan <102892407+itsrcx@users.noreply.github.com>
2024-09-01 19:24:48 +0530

    Update urls.py

--- src/storyKeeper/urls.py
+++ src/storyKeeper/urls.py
@@ -34,7 +34,8 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]  
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # for the openapi Schema >>>>>>>>>>>>>>>>>>>>>>>>>>
     # path('openapi', get_schema_view( # new
     #     title="Blog API",
