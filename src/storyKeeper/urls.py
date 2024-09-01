416a982ce87ea6d05ca8f1aebbc9aa0191f13f6d
Raman Chauhan <102892407+itsrcx@users.noreply.github.com>
2024-09-01 19:22:12 +0530

    Update urls.py

--- src/storyKeeper/urls.py
+++ src/storyKeeper/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view 
 from rest_framework import permissions
+from django.conf import settings
 
 # for open api schema
 schema_view = get_schema_view(
@@ -33,7 +34,7 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # for the openapi Schema >>>>>>>>>>>>>>>>>>>>>>>>>>
     # path('openapi', get_schema_view( # new
     #     title="Blog API",
