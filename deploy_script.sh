72a63f4ef04396991426bd4ccb5b4aea7592d312
joyboy <ramanchauhanxx@gmail.com>
2024-09-12 15:02:31 +0530

    ci-cd :::

--- deploy_script.sh
+++ deploy_script.sh
@@ -2,17 +2,14 @@
 
 DRY_RUN=$1
 
-if [ ! -d "/home/ubuntu/online-book-store" ]; then
-  echo "Directory /home/ubuntu/online-book-store does not exist."
-  exit 1
-fi
-echo "Directory /home/ubuntu/online-book-store does not exist."
 echo "Change directory to repo..."
 [ "$DRY_RUN" != "true" ] && cd /home/ubuntu/online-book-store
 
+echo "Directory: $(pwd)"
+
 echo "Pulling latest code from repository..."
 # Skip actual git pull in dry run
-[ "$DRY_RUN" != "true" ] && git pull
+[ "$DRY_RUN" != "true" ] && git pull origin master
 
 echo "Installing dependencies..."
 # Skip actual installation in dry run
